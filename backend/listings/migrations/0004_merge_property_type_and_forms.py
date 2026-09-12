bda1cbf8d480ccece2bf49f4d4fc84970ab89577
armi <bahaaeddinebouzid@gmail.com>
2026-09-12 14:24:19 +0100

    merge listing

---
+++ backend/listings/migrations/0004_merge_property_type_and_forms.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("listings", "0003_contactmessage_estimationrequest_and_more"),
+        ("listings", "0003_property_property_type"),
+    ]
+
+    operations = []
